59b38573f1a8b731e1e0985b57d96a601f1feaed
Paulius <paulius000@gmail.com>
2024-04-11 18:54:18 +0300

    another: try at cors()

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { corsMiddleware } from "./middleware/corsMiddleware.js";
+import cors from "cors";
+// import { corsMiddleware } from "./middleware/corsMiddleware.js";
 import { rateLimiterMiddleware } from "./middleware/rateLimiterMiddleware.js";
 
 import { proxyRouter } from "./routes/proxy.routes.js";
@@ -7,15 +8,27 @@ import { historyRouter } from "./routes/history.routes.js";
 import { loginRouter } from "./routes/login.routes.js";
 
 const app = express();
+
 const allowedOrigins: originType = [
 	"https://watcher-owl.vercel.app/",
 	"http://localhost:1337",
 ];
 
-app.use(corsMiddleware(allowedOrigins));
-app.use("/api", rateLimiterMiddleware);
+app.use(
+	cors({
+		origin: allowedOrigins,
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+		credentials: true,
+	})
+);
+// app.use(corsMiddleware(allowedOrigins));
+
+app.options("*", cors());
 
 app.use(express.json());
+
+app.use("/api", rateLimiterMiddleware);
 
 app.use("/api", proxyRouter);
 app.use("/api", historyRouter);
