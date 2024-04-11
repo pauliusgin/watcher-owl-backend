c9e9d53d2c51f52c7e82818ccbe575ca1aa4a9e4
Paulius <paulius000@gmail.com>
2024-04-11 18:46:09 +0300

    change: try cors middleware

--- src/middleware/corsMiddleware.ts
+++ src/middleware/corsMiddleware.ts
@@ -1,12 +1,13 @@
 import cors from "cors";
 
 function corsMiddleware(allowedOrigins: originType) {
-	const corsOptions = allowedOrigins
-		? {
-				origin: allowedOrigins,
-				optionSuccessStatus: 200,
-			}
-		: {};
+	const corsOptions = {
+		origin: allowedOrigins,
+		optionSuccessStatus: 200,
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+		credentials: true,
+	};
 	return cors(corsOptions);
 }
 
